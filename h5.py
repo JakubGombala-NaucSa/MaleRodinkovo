336a8fd836d59fef47eda375af9c745ebe5a818e
JakubGombala-NaucSa <spam.ucet4713@gmail.com>
2025-04-08 16:46:19 +0200

    Update h5.py

--- h5.py
+++ h5.py
@@ -14,25 +14,33 @@ farby = ["yellow", "red", "blue","green","orange", "pink"]
 
 
 def Animacia():
-    global nahoda, farby, ConstNahoda
-    platno.delete("num0")
-    platno.delete("num1")
-    platno.delete("num2")
-    
-    pole = []
-    for i in range(3):
+    global nahoda, farby, ConstNahoda, vysledky
+    if ConstNahoda - nahoda < ConstNahoda/3:
+        platno.delete("num0")
         cislo = randint(1,9)
-        pole.append(cislo)
-        platno.create_text(100 + i*150, 150, fill=farby[randint(0,5)], text=cislo, font=("Purisa", 50), tag="num"+str(i))
+        platno.create_text(100, 150, fill=farby[randint(0,5)], text=cislo, font=("Purisa", 50), tag="num0")
+        vysledky[0] = cislo
+    if ConstNahoda - nahoda < ConstNahoda*2/3:
+        platno.delete("num1")
+        cislo = randint(1,9)
+        platno.create_text(250, 150, fill=farby[randint(0,5)], text=cislo, font=("Purisa", 50), tag="num1")
+        vysledky[1] = cislo
+    if ConstNahoda - nahoda < ConstNahoda:
+        platno.delete("num2")
+        cislo = randint(1,9)
+        platno.create_text(400, 150, fill=farby[randint(0,5)], text=cislo, font=("Purisa", 50), tag="num2")
+        vysledky[2] = cislo
+        
     if nahoda == 0:
-        Vysledok(pole)
+        Vysledok(vysledky)
         return
     nahoda -= 1
 
     platno.after(100, Animacia)
 
 def Start():
-    global nahoda, ConstNahoda, peniaze
+    global nahoda, ConstNahoda, peniaze, vysledky
+    vysledky = [0,0,0]
     platno.delete("p")
     peniaze -= 1
     nahoda = randint(2,10)
@@ -42,8 +50,18 @@ def Start():
     Animacia()    
 
 def Vysledok(roll):
-    if roll[0] == roll[1] == roll[2]:
-        print("yayyy")
+    if roll[0] == roll[1] == roll[2] and roll[0] == 7:
+        peniaze += 10
+    elif roll[0] == roll[1] == roll[2]:
+        peniaze += 3
+    elif roll[0]+2 == roll[1]+1 == roll[2]:
+        peniaze += 6
+    elif ((roll[0] == roll[1] and roll[0] == 7) or
+         (roll[0] == roll[2] and roll[0] == 7) or
+         (roll[1] == roll[2] and roll[1] == 7)):
+        peniaze += 5
+    elif roll[0] == 7 or roll[1] == 7 or roll[2] == 7:
+        peniaze += 1
     else:
         print("Nayy")
     
